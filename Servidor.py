9c1734c9bf0f0881ff9cf7876975ce61a5db0b6f
Z2ppbWVuZXpkZXph <62645524+Z2ppbWVuZXpkZXph@users.noreply.github.com>
2021-06-25 23:25:29 -0500

    Servidor,py
    Este archivo se encaga de crear el servidor respectivamente.

---
+++ Servidor.py
@@ -0,0 +1,14 @@
+import socket
+
+servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+servidor.bind(('127.0.0.3', 444))
+servidor.listen(5)
+
+while True:
+       cliente, direccion = servidor.accept()
+       print('Acepta conexión del cliente', direccion)
+       mensaje_codificado = cliente.recv(1024)
+       mensaje_decodificado = mensaje_codificado.decode('utf-8')
+       print('Mensaje recibido:', mensaje_decodificado)
+       cliente.send(b'Gracias por conectarte')
+       cliente.close()
