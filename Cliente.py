0c6a932f48182a6d806309b70ce76aff80fe6c1c
Z2ppbWVuZXpkZXph <62645524+Z2ppbWVuZXpkZXph@users.noreply.github.com>
2021-06-27 12:23:22 -0500

    Update Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -1,11 +1,15 @@
 import socket
 
 servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-servidor.connect(("127.0.0.3", 4000))
-mensaje = input("Manda un mensaje: ")
-mensaje_listo = str.encode(mensaje, "utf-8")
-servidor.send(mensaje_listo)
+servidor.connect(("192.168.1.43", 4002))
 mensaje_recibido = servidor.recv(1024)
 mensaje_decodificado = mensaje_recibido.decode("utf-8")
-print ("Mensaje recibido:", mensaje_decodificado)
+print (mensaje_decodificado)
+while True:
+	mensaje = input("Manda un mensaje: ")
+	mensaje_listo = str.encode(mensaje, "utf-8")
+	servidor.send(mensaje_listo)
+	mensaje_recibido = servidor.recv(1024)
+	mensaje_decodificado = mensaje_recibido.decode("utf-8")
+	print ("Mensaje recibido:", mensaje_decodificado)
 servidor.close()
